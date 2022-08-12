924d2d7798afe26368f72f277fcd7ee302821663
ji-0630 <65642065+ji-0630@users.noreply.github.com>
2022-08-12 11:27:08 +0900

    Create plant.kt

---
+++ plant.kt
@@ -0,0 +1,6 @@
+package com.example.plantdiary
+
+/*result 파일에서 불러오는 plant에 대한 데이터클래스*/
+
+class plant (val img: String, val name: String, val manag: String, val func: String,
+             val water: String, val sun : String, val display: String)
