91af5addec4c8fbd2c27a53b01edc60bcb8f41ed
ji-0630 <65642065+ji-0630@users.noreply.github.com>
2022-09-10 23:20:32 +0900

    Create login.kt

---
+++ login.kt
@@ -0,0 +1,64 @@
+package com.example.plantdiary
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        btn_login.setOnClickListener{
+            signinAndsignup()
+        }
+    }
+
+    fun signinAndsignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful){
+                    // create user account
+                    moveUserPage()
+                }else if (task.exception?.message.isNullOrEmpty()) {
+                    // show error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }else {
+                    //login if you have account
+                    signinEmail()
+                }
+
+            }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                        if (task.isSuccessful) {
+                            // Login
+                            moveMainPage(task.result?.user)
+                        }else {
+                            // show the error message
+                            Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+    }
+    fun moveUserPage(){
+            startActivity(Intent(this, User::class.java))
+    }
+
+
+    fun moveMainPage(user:FirebaseUser?) {
+        if(user != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
